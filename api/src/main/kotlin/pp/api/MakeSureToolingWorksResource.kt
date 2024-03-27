7c925b4277ac03d7fc584a04be80aeecced177cc
Cornelius Wichering <cornelius.wichering@gmail.com>
2024-03-27 10:06:38 +0100

    build: add project code for api
    There's no "production" code yet, only one `MakeSureToolingWorksResource`

---
+++ api/src/main/kotlin/pp/api/MakeSureToolingWorksResource.kt
@@ -0,0 +1,17 @@
+package pp.api
+
+import jakarta.ws.rs.GET
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.core.MediaType.TEXT_PLAIN
+
+/**
+ * This class' only purpose is to make sure our tooling works.
+ */
+@Path("/hello")
+class MakeSureToolingWorksResource {
+
+    @GET
+    @Produces(TEXT_PLAIN)
+    fun hello() = "hello"
+}

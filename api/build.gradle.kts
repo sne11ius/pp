a890d2c1ee5104a04cd6f293f34cf7d4d55224fe
Cornelius Wichering <cornelius.wichering@gmail.com>
2024-03-27 10:06:38 +0100

    build: add project code for api
    There's no "production" code yet, only one `MakeSureToolingWorksResource`

---
+++ api/build.gradle.kts
@@ -0,0 +1,51 @@
+import org.gradle.api.JavaVersion.VERSION_21
+
+plugins {
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.allopen") version "1.9.22"
+    id("io.quarkus")
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+val quarkusPlatformGroupId: String by project
+val quarkusPlatformArtifactId: String by project
+val quarkusPlatformVersion: String by project
+
+dependencies {
+    implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
+    implementation("io.quarkus:quarkus-kotlin")
+    implementation("io.quarkus:quarkus-resteasy-reactive")
+    implementation("io.quarkus:quarkus-websockets")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.quarkus:quarkus-arc")
+    testImplementation("io.quarkus:quarkus-junit5")
+    testImplementation("io.rest-assured:rest-assured")
+}
+
+group = "pp"
+version = "1.0.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = VERSION_21
+    targetCompatibility = VERSION_21
+}
+
+tasks.withType<Test> {
+    systemProperty("java.util.logging.manager", "org.jboss.logmanager.LogManager")
+}
+
+allOpen {
+    annotation("jakarta.ws.rs.Path")
+    annotation("jakarta.enterprise.context.ApplicationScoped")
+    annotation("jakarta.persistence.Entity")
+    annotation("io.quarkus.test.junit.QuarkusTest")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = VERSION_21.toString()
+    kotlinOptions.javaParameters = true
+}
---
+++ api/src/test/kotlin/pp/api/MakeSureToolingWorksResourceTest.kt
@@ -0,0 +1,26 @@
+package pp.api
+
+import io.quarkus.test.junit.QuarkusTest
+import io.restassured.RestAssured.given
+import io.restassured.RestAssured.registerParser
+import io.restassured.parsing.Parser.TEXT
+import org.hamcrest.Matchers.`is`
+import org.junit.jupiter.api.Test
+
+/**
+ * This tests only purpose is to make sure our tooling works.
+ */
+@QuarkusTest
+class MakeSureToolingWorksResourceTest {
+
+    @Test
+    fun testHelloEndpoint() {
+        registerParser("text/plain", TEXT);
+        given()
+            .`when`().get("/hello")
+            .then()
+            .statusCode(200)
+            .body(`is`("hello"))
+    }
+
+}

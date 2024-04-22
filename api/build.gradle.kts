09ed56616ff960060c2afd9ce986c502b0fe8240
Cornelius Wichering <cornelius.wichering@gmail.com>
2024-04-23 01:11:00 +0200

    chore: release main

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -39,7 +39,7 @@ group = "pp"
 // Please do not change the following marker comments as they are used by the "release-please" github action.
 // See https://github.com/googleapis/release-please/blob/main/docs/customizing.md#updating-arbitrary-files
 // x-release-please-start-version
-version = "0.0.2"
+version = "0.0.3"
 // x-release-please-end
 
 java {
